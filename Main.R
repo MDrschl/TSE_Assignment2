a23c2ecd0155ff30e8e45ab3b63194a9d6ce3b37
MDrschl <117458298+MDrschl@users.noreply.github.com>
2025-03-09 19:05:47 +0100

    changes in Main.R

--- Main.R
+++ Main.R
@@ -15,32 +15,33 @@ K <- 1000 # Number of trajectories
 N <- 100 # In-sample size
 H <- 3 # Forecast horizon
 
-# simulate MA(3) process
+# coefficients of MA(3)
 ma.coeffs <- c(-0.5, 0.3, -0.2)
-x <- replicate(K, arima.sim(model = list(ma = ma.coeffs), n = N + H))
+# creating series of errors for each trajectory
+errors <- matrix(rnorm(K * (N + H)), nrow = N + H, ncol = K)
 
-errors <- matrix(0, nrow = N + H, ncol = K)
-
+# Simulate MA(3) process using the generated innovations and the coefficient vector
+x <- matrix(0, nrow = N + H, ncol = K)
 for (k in 1:K) {
-  for (t in 4:(N + H)) {
-    errors[t, k] <- x[t, k] - sum(ma.coeffs * errors[(t-1):(t-3), k], na.rm = TRUE)
-  }
+  x[, k] <- arima.sim(model = list(ma = ma.coeffs), n = N + H, innov = errors[, k])
 }
 
 ### b) Optimal MSE Forecasts ----
 
-optimal.fc <- array(0, dim = c(N, H, K))
+# initiate matrix for optimal MSE forecast
+optimal.fc <- matrix(0, nrow = N + H, ncol = K)
 
 for (k in 1:K) {
   for (t in 3:N) {
-    optimal.fc[t, 1, k] <- ma.coeffs[1] * errors[t, k] + ma.coeffs[2] * errors[t-1, k] + ma.coeffs[3] * errors[t-2, k]
-    optimal.fc[t, 2, k] <- ma.coeffs[2] * errors[t, k] + ma.coeffs[3] * errors[t-1, k]
-    optimal.fc[t, 3, k] <- ma.coeffs[3] * errors[t, k]
+    optimal.fc[t + 1, k] <- ma.coeffs[1] * errors[t, k] + ma.coeffs[2] * errors[t - 1, k] + ma.coeffs[3] * errors[t - 2, k]
+    if (t + 2 <= N + H) {
+      optimal.fc[t + 2, k] <- ma.coeffs[2] * errors[t, k] + ma.coeffs[3] * errors[t - 1, k]
+    }
+    if (t + 3 <= N + H) {
+      optimal.fc[t + 3, k] <- ma.coeffs[3] * errors[t, k]
+    }
   }
 }
-
-# Print first few forecasts for trajectory 1
-print(optimal.fc[1:10, , 1])
 
 
 # Initiate a vector for the MSE for the H forecast periods
@@ -52,41 +53,41 @@ for (h in 1:H) {
   # initiate an vector to store error terms
   errors.squ <- numeric(K)
   
-  # loop over each trajectory
+  # loop over each trajectory and caluclate mean squared error
   for (k in 1:K) {
-    # for each period to forecast, subtract the forecast from the previous period
-    errors.fc <- x[(N + h), k] - (x[N,k] + optimal.fc[N,h,k])
-    # square result
+    errors.fc <- x[(N + h), k] - optimal.fc[(N + h), k]
     errors.squ[k] <- errors.fc^2
   }
-  
-  # take mean over all trajectories
   optimal.mse[h] <- mean(errors.squ)
 }
 
 print(optimal.mse)
 
-
 ### c) Approach 1: Naive Error Sequence Reconstruction ----
 
-err.recon <- matrix(0, nrow = N + H + 2, ncol = K)
+# initiate matrix for reconstructed errors
+err.recon <- matrix(0, nrow = N + H, ncol = K)
 
-approach1.fc <- array(0, dim = c(N, H, K))
+approach1.fc <- matrix(0, nrow = N + H, ncol = K)
 
 for (k in 1:K) {
   
   err.recon[1, k] <- x[1, k]
-  err.recon[2, k] <- x[2, k] - ma.coeffs[1]*err.recon[1,k]
-  err.recon[3, k] <- x[3, k] - ma.coeffs[1]*err.recon[2, k] - ma.coeffs[2] * err.recon[1, k]
+  err.recon[2, k] <- x[2, k] - ma.coeffs[1] * err.recon[1, k]
+  err.recon[3, k] <- x[3, k] - ma.coeffs[1] * err.recon[2, k] - ma.coeffs[2] * err.recon[1, k]
   
-  for (t in 4:N) { 
-    err.recon[t, k] <- x[t, k] - sum(ma.coeffs * err.recon[(t-1):(t-3), k], na.rm = TRUE)
+  for (t in 4:(N)) {
+    err.recon[t, k] <- x[t, k] - sum(ma.coeffs * err.recon[(t - 1):(t - 3), k], na.rm = TRUE)
   }
   
-  for (t in 4:N) {
-    approach1.fc[t, 1, k] <- ma.coeffs[1] * err.recon[t, k] + ma.coeffs[2] * err.recon[t-1, k] + ma.coeffs[3] * err.recon[t-2, k]
-    approach1.fc[t, 2, k] <- ma.coeffs[2] * err.recon[t, k] + ma.coeffs[3] * err.recon[t-1, k]
-    approach1.fc[t, 3, k] <- ma.coeffs[3] * err.recon[t, k]
+  for (t in 3:N) {
+    approach1.fc[t + 1, k] <- ma.coeffs[1] * err.recon[t, k] + ma.coeffs[2] * err.recon[t - 1, k] + ma.coeffs[3] * err.recon[t - 2, k]
+    if (t + 2 <= N + H) {
+      approach1.fc[t + 2, k] <- ma.coeffs[2] * err.recon[t, k] + ma.coeffs[3] * err.recon[t - 1, k]
+    }
+    if (t + 3 <= N + H) {
+      approach1.fc[t + 3, k] <- ma.coeffs[3] * err.recon[t, k]
+    }
   }
 }
 
@@ -95,11 +96,9 @@ approach1.mse <- numeric(H)
 for (h in 1:H) {
   errors.squ <- numeric(K)
   for (k in 1:K) {
-
-    errors.fc <- x[N + h, k] - (x[N, k] + approach1.fc[N, h, k])
+    errors.fc <- x[N + h, k] - approach1.fc[N + h, k] # Corrected line
     errors.squ[k] <- errors.fc^2
   }
-  
   approach1.mse[h] <- mean(errors.squ)
 }
 
